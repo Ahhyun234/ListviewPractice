ee75b5aa3aaf5bf0f3e906540ac935fb865ae00e
이아현 <suk03029@gmail.com>
2021-11-03 12:49:24 +0900

    학생 목록을 뿌려주는 Adpter의 기초 작업 중

---
+++ app/src/main/java/com/nepplus/listviewpractice/adapters/Student_adapter.kt
@@ -0,0 +1,12 @@
+package com.nepplus.listviewpractice.adapters
+
+import android.content.Context
+import android.content.LocusId
+import android.widget.ArrayAdapter
+import com.nepplus.listviewpractice.datas.StudentData
+
+class Student_adapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StudentData> ) : ArrayAdapter<StudentData>() {
+}
