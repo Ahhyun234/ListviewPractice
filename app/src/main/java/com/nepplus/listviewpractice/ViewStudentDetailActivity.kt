c2d46ea21df6d2012db7e6949a270855bb50d054
이아현 <suk03029@gmail.com>
2021-11-03 17:04:19 +0900

    학생 상세보기/ putextra/ getextra 구현
    getIntextra는 기본 값이 필요한 경우도 있다

--- app/src/main/java/com/nepplus/listviewpractice/ViewStudentDetailActivity.kt
+++ app/src/main/java/com/nepplus/listviewpractice/ViewStudentDetailActivity.kt
@@ -2,10 +2,24 @@ package com.nepplus.listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_student_detail.*
+
 
 class ViewStudentDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_student_detail)
+
+
+
+        val name = intent.getStringExtra("name")
+        txtName.text = name
+
+        val address = intent.getStringExtra("address")
+        txtAddress.text=address
+
+        val birthYear = intent.getIntExtra("birthYear")
+        txtAge.text=birthYear
+
     }
 }
