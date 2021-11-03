131ec3ac44421d3811541e26309911eb8d0cba5c
이아현 <suk03029@gmail.com>
2021-11-03 13:21:46 +0900

    실제 어댑터를 화면에서 사용할 준비를 해놓고 리스트 뷰의 어댑터로 우리가 만든 어댑터 연결

--- app/src/main/java/com/nepplus/listviewpractice/MainActivity.kt
+++ app/src/main/java/com/nepplus/listviewpractice/MainActivity.kt
@@ -2,10 +2,27 @@ package com.nepplus.listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.listviewpractice.adapters.Student_adapter
+import com.nepplus.listviewpractice.datas.StudentData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<studentData>()
+    lateinit var mStudent_adapter: Student_adapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(StudentData("이아현",1996,"서울시동대문구"))
+        mStudentList.add(StudentData("조경진",1988,"서울시동대문구"))
+        mStudentList.add(StudentData("김준기",1996,"경기도 남양주"))
+        mStudentList.add(StudentData("방우진",1983,"경기도 고양시"))
+        mStudentList.add(StudentData("이지원",1993,"경기도 관악구"))
+        mStudentList.add(StudentData("이지원",1993,"경기도 관악구"))
+
+        mStudent_adapter = StudentAdapter(this, R.layout.student_list_item,mStudentList)
+        studentListView.adapter = mStudent_adapter
     }
 }
