9ac40001027447401a6e4a8a5defbcddb3566520
이아현 <suk03029@gmail.com>
2021-11-03 13:01:41 +0900

    ConvertView를 tempLow에 옮겨담아서 비어있는 상태라면 채워주기

--- app/src/main/java/com/nepplus/listviewpractice/adapters/Student_adapter.kt
+++ app/src/main/java/com/nepplus/listviewpractice/adapters/Student_adapter.kt
@@ -2,7 +2,11 @@ package com.nepplus.listviewpractice.adapters
 
 import android.content.Context
 import android.content.LocusId
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.nepplus.listviewpractice.R
 import com.nepplus.listviewpractice.datas.StudentData
 
 class Student_adapter(
@@ -10,4 +14,15 @@ class Student_adapter(
     val resId: Int,
     val mList: ArrayList<StudentData> )
     : ArrayAdapter<StudentData>(mContext, resId, mList) {
+
+        val mInflater = LayoutInflater.from(mContext)
+
+//    자동으로 생성 된 return super를 버렸음
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+    val tempLow  = convertView
+    if(tempLow == null){
+        tempLow =mInflater. inflate(R.layout.student_list_item,null)
+    }
+    }
 }
